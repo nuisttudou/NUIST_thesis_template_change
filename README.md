20a80291509d28fa407a18a9d315aefbd495e5d6
tudou <20171308085@nuist.edu.cn>
2021-01-06 20:01:18 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# NUIST_thesis_template_change
+修改了https://github.com/LirenW/NUIST_thesis_template_V2.0
+解决封面的选中时，会出现莫名其妙的内容的问题
---
+++ clean.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+rm -rf ctextemp_*.* 
+rm -rf ctextemp_*.*.* 
+rm -rf *.*.bak 
+rm -rf *.log 
+rm -rf *.aux 
+rm -rf *.toc 
+rm -rf *.dvi 
+rm -rf *.thm 
+rm -rf *.out 
+rm -rf *.gz 
+
+rm -rf *.fdb_latexmk 
+rm -rf *.fls
+rm -rf *.xdv
